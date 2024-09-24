a9bdea56a26ca81264d27e9d7a8f7a498d028609
Eli OCCANSEY <62498537+elioccansey@users.noreply.github.com>
2024-09-24 16:06:17 -0500

    Delete server-music-player-app directory

--- server-music-player-app/src/routes/SongRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as SongController from "../controller/SongController";
-
-const router = Router();
-
-router.get("/all", SongController.getAllSongs);
-
-export default router;
